ccd9cce9b0b8ba98e0f09bb78cf2b4b907c7d654
ahoward <ara.t.howard@gmail.com>
2013-02-21 11:13:09 -0700

    update cap recipe

--- lib/sekrets/capistrano.rb
+++ lib/sekrets/capistrano.rb
@@ -1,19 +1,17 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :sekrets do
     task :upload_key do
-      require 'fileutils'
-
       rails_root = File.expand_path(File.dirname(__FILE__))
 
-      src = File.join(rails_root, 'sekrets.key')
-      dst = File.join(deploy_to, 'sekrets.key')
+      src = File.join(rails_root, '.sekrets.key')
+      dst = File.join(deploy_to, 'current', '.sekrets.key')
 
       if test(?s, src)
         upload(src, dst, :recursive => true)
       end
     end
   end
-        
+
 
   before "deploy:finalize_update", "sekrets:upload_key"
 end
